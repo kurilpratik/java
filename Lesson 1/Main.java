eb7d68522377fbf6b04d1156da691ba5c1dc4f4a
Pratik Kuril <pratikkuril16@gmail.com>
2025-02-02 23:16:08 +0530

    Set up repo

---
+++ Lesson 1/Main.java
@@ -0,0 +1,10 @@
+// If we have a file Main.java, it will contain a class Main
+// Any vairable starting with a capital letter in any language is a class (convention)
+// Convention - Name of the class must start with a capital letter
+// The named class of the file should be public (convention)
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");   
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Learning Java
+
+Learning Java by Kunal Kushwaha. [View lectures](https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ)
